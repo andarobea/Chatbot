b0a298390a9ebcea05ce2421989af7b144f022e6
Anda Robea <anda_teodora.robea@stud.acs.upb.ro>
2024-12-14 01:52:54 +0200

    updated train.py

--- run.sh
+++ run.sh
@@ -3,7 +3,12 @@
 # Activate the virtual environment
 source venv/bin/activate
 
-# Run the Flask server
+# Run the training script
+echo "Starting training..."
+python train.py
+
+# Start the Flask server
+echo "Starting the Flask server..."
 export FLASK_APP=app.py  # Replace with your Flask application filename
 export FLASK_ENV=development  # Optional: Enables debug mode
 flask run
