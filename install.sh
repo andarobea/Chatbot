8eddd17cc5d6307883d3cea125b04f38f4dd63cc
Anda Robea <anda_teodora.robea@stud.acs.upb.ro>
2024-12-20 00:02:26 +0200

    updated installation

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # Create and activate a virtual environment
+
 python3.10 -m venv venv
 . venv/bin/activate
 
 # Install required Python packages
 pip install --upgrade pip  # Optional: Upgrade pip
-pip install nltk numpy Flask torch torchvision random
+pip install nltk
+pip install numpy
+pip install Flask
+pip install torch
+pip install torchvision
+pip install random2
 
 echo "Installation complete. Activate the virtual environment with 'source venv/bin/activate' and use './run.sh' to start the server."
